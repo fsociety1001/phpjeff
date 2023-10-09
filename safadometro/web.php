203426ea9d5e7fb850f0ca4084947b7a1531ae25
fsociety <143713077+fsociety1001@users.noreply.github.com>
2023-10-08 23:24:03 -0300

    web.php

---
+++ safadometro/web.php
@@ -0,0 +1,19 @@
+<h1>Safadômetro</h1>
+
+<form action="safadometro.php">
+
+    <input type="text" placeholder="Informe o dia : " name="campoDia">
+    <br/>
+    <br/>
+    <input type="text" placeholder="inform o mês : " name="campoMes">
+    <br/>
+    <br/>
+    <input type="text" placeholder="inform o ano : " name="campoAno">
+    <br/>
+    <br/>
+
+    <input type="submit" value="calculate">
+
+</form>
+
+<img src="https://i.pinimg.com/550x/9c/9a/a2/9c9aa264b49d10a607b6b7fd84a003c3.jpg">
