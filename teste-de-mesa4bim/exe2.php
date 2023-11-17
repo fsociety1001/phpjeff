9c40ee4c65857940651777728548f817ec86d3d4
fsociety <143713077+fsociety1001@users.noreply.github.com>
2023-11-17 14:46:42 -0300

    exe2.php

---
+++ teste-de-mesa4bim/exe2.php
@@ -0,0 +1,25 @@
+<?php
+
+$sete_maravilhas = [
+"Cristo Redentor",
+"Chichén Itzá",
+"Coliseu",
+"Muralha da China",
+"Petra",
+"Machu Picchu",
+"Taj Mahal"
+];
+
+    for($i=0; $i < 7; $i++){
+        print($i+1 . ": ". $sete_maravilhas[$i]. "\n");
+}
+//terminal:
+/**
+* 1: Cristo Redentor
+* 2: Chichén Itzá
+* 3: Coliseu
+* 4: Muralha da China
+* 5: Petra
+* 6: Machu Picchu
+* 7: Taj Mahal
+ */
