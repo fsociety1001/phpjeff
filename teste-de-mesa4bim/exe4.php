37a05d835675e6baec677b2736618f1372f1196e
fsociety <143713077+fsociety1001@users.noreply.github.com>
2023-11-18 12:35:49 -0300

    exe4.php

---
+++ teste-de-mesa4bim/exe4.php
@@ -0,0 +1,20 @@
+<?php
+
+    $mega_sena = [33, 4, 47, 7, 11, 25];
+    $aux = 0;
+
+    for($i = 0; $i < 6; $i++) {
+        for($j = 0; $j < 5; $j++) {
+
+            if($mega_sena[$j] > $mega_sena[$j + 1]) {
+                $aux = $mega_sena[$j];
+                $mega_sena[$j] = $mega_sena[$j + 1];
+                $mega_sena[$j + 1] = $aux;
+            }
+        }
+    }
+
+    for ($i = 0; $i < 6; $i++) {
+        print($mega_sena[$i] . " - ");
+    }
+?>
