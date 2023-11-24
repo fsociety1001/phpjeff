87f8a3559b7e15af57081faa9d2d0e1ade2eb846
Arthur <143713077+olhaarthur@users.noreply.github.com>
2023-11-24 09:39:55 -0300

    exe9.php

--- teste-de-mesa4bim/exe9.php
+++ teste-de-mesa4bim/exe9.php
@@ -8,37 +8,33 @@ $pessoa = [
     'profissao'=> 'Astronauta',
 ];
 
-print "Teste 0: ";
-var_dump(in_array('Físico', $pessoa));
-
-print "Teste 1: ";
-var_dump(in_array("Detetive", $pessoa));
-
-print "Teste 3: ";
-var_dump(array_search("nome", $pessoa));
-
-print   "Teste 4: ";
-var_dump(array_key_exists("profissão", $pessoa));
-
-print "Teste 5: ";
-var_dump(array_key_first($pessoa));
-
-print"Teste 6: ";
-var_dump(array_key_last($pessoa));
-
-print("Teste 7: ");
-var_dump(sort($pessoa));
-
+ print "Teste 0: ";
+        var_dump(count($pessoa));
+    print "\nTeste 1: ";
+        var_dump(in_array('Físico', $pessoa));
+    print "\nTeste 2: ";
+            var_dump(in_array('Detetive', $pessoa));
+    print "\nTeste 3: ";
+        var_dump(array_search('nome', $pessoa));
+    print "\nTeste 4: ";
+        var_dump(array_key_exists('profissao', $pessoa));
+    print "\nTeste 5: ";
+        var_dump(array_key_first($pessoa));
+    print "\nTeste 6: ";
+        var_dump(array_key_last($pessoa));
+    print "\nTeste 7: ";
+        var_dump(sort($pessoa));
 ?>
 
 //console:
 
-/**
- Teste 0: bool(true)      
-Teste 1: bool(false)     
-Teste 3: bool(false)     
-Teste 4: bool(false)     
-Teste 5: string(4) "nome"
-Teste 6: string(9) "profissao"
-Teste 7: bool(true)
+/*
+Teste 0: int(5) --->  teste 0
+Teste 1: bool(true) ---> verifyString("fisico") in array() = true.
+Teste 2: bool(false) ---> verifyString("detetive") in array() = false.
+Teste 3: bool(false) ---> verifyString("nome"). error: "nome" = *key
+Teste 4: bool(true) ---> verifyKey("profissao")  = true.
+Teste 5: string(4) "nome"  ---> echoKey("nome") string 4 caracteres
+Teste 6: string(9) "profissao" ---> shoWkey("profissao") string 9 caract.
+Teste 7: bool(true) ---> sort efetuado.
 */
